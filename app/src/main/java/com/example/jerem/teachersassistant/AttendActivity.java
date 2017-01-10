824bf164fa1bc8981714b5bbf17484760c6db6f6
jerem <jeremrajsamuel@gmail.com>
2017-01-10 15:38:19 +0530

    3:45,10-1-2017

--- app/src/main/java/com/example/jerem/teachersassistant/AttendActivity.java
+++ app/src/main/java/com/example/jerem/teachersassistant/AttendActivity.java
@@ -1,6 +1,5 @@
 package com.example.jerem.teachersassistant;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,21 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class AttendanceActivity extends AppCompatActivity {
-Intent i;
+public class AttendActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_attendance);
+        setContentView(R.layout.presentabsent);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-    }
-    public void aar(View v) {
-
-        i = new Intent(AttendanceActivity.this, AttendActivity.class);
-        startActivity(i);
+        
     }
 
 }
