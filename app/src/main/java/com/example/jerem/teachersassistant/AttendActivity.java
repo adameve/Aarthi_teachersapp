b135f51889f79c9f2d135aedbb8972bcd6d014ec
jerem <jeremrajsamuel@gmail.com>
2017-01-24 15:44:56 +0530

    24-1-2017 attendance

--- app/src/main/java/com/example/jerem/teachersassistant/AttendActivity.java
+++ app/src/main/java/com/example/jerem/teachersassistant/AttendActivity.java
@@ -2,12 +2,43 @@ package com.example.jerem.teachersassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AttendActivity extends AppCompatActivity {
 
+    public RecyclerView mRecycleView;
+    public AttendanceAdapter mAdapter;
+    public ArrayList<String> mArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attend);
+        mRecycleView = (RecyclerView) findViewById(R.id.recycler_view);
+        mArrayList  = new ArrayList<>();
+        mArrayList.add("Jerry");
+        mArrayList.add("Aarthi");
+        mArrayList.add("Vinith");
+        mArrayList.add("giri pragash");
+        mArrayList.add("keerthi");
+        mArrayList.add("ganga");
+        mArrayList.add("gowsalya");
+        mArrayList.add("gopi kannan");
+        mArrayList.add("Akshykanna");
+        mArrayList.add("karthick");
+        mArrayList.add("Aishwarya");
+        mArrayList.add("AncyMeja");
+        mArrayList.add("Prasad thangavel");
+        mAdapter    = new AttendanceAdapter(this,mArrayList);
+        mRecycleView.setLayoutManager(new GridLayoutManager(this,3));
+        mRecycleView.setAdapter(mAdapter);
+    }
+    public void stu (View v){
+        Toast.makeText(getApplicationContext(), "Attendance updated", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/jerem/teachersassistant/AttendanceAdapter.java
+++ app/src/main/java/com/example/jerem/teachersassistant/AttendanceAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -12,34 +14,42 @@ import java.util.ArrayList;
  * Created by jerem on 24-01-2017.
  */
 
-public class AttendanceAdapter  extends RecyclerView.Adapter<AttendanceAdapter.AttendanceViewHolder> {
-Context context;
+public class AttendanceAdapter extends RecyclerView.Adapter<AttendanceAdapter.AttendanceViewHolder> {
+    Context context;
     ArrayList<String> attendenceArray;
 
-    public AttendanceAdapter(Context context, ArrayList<String> attendenceArray){
-this.context = context;
-        this.attendenceArray=attendenceArray;
+    public AttendanceAdapter(Context context, ArrayList<String> attendenceArray) {
+        this.context = context;
+        this.attendenceArray = attendenceArray;
     }
+
     @Override
     public AttendanceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_attendance,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_attendance, parent, false);
+
         return new AttendanceViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(AttendanceViewHolder holder, int position) {
+        holder.mStudentRollNo.setText(attendenceArray.get(position).toString());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return attendenceArray.size();
     }
 
-    public class AttendanceViewHolder extends RecyclerView.ViewHolder{
+    public class AttendanceViewHolder extends RecyclerView.ViewHolder {
+        ImageView mStudentImage;
+        TextView mStudentRollNo;
 
         public AttendanceViewHolder(View itemView) {
             super(itemView);
+            mStudentImage = (ImageView) itemView.findViewById(R.id.student_image);
+            mStudentRollNo = (TextView) itemView.findViewById(R.id.student_rno);
+
         }
     }
 }
